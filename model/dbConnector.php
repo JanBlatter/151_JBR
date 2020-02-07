d227452abf660387c55f3ec6ba205e606cd4e7b3
JanBlatterCPNV <48351896+JanBlatterCPNV@users.noreply.github.com>
2020-02-07 11:32:39 +0100

    création dbConnector

---
+++ model/dbConnector.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * 151_2019_startproject - dbConnector.php
+ * User: Jan.BLATTER
+ * Date: 07.02.2020
+ */
+
+
+//Test client - must be remove to before using in a project
+//try to get a list of available tables in database
+$query = 'SHOW TABLES';
+$queryResult = executeQuery($query);
+
+//Source : http://php.net/manual/en/pdo.prepare.php
+function executeQuery($query)
+{
+    $queryResult = null;
+
+    $dbConnexion = openDBConnexion();//open database connexion
+    if ($dbConnexion != null) {
+        $statement = $dbConnexion->prepare($query);//prepare query
+        $statement->execute();//execute query
+        $queryResult = $statement->fetchAll();//prepare result for client
+    }
+    $dbConnexion = null;//close database connexion
+    return $queryResult;
+}
+
+//Source : http://php.net/manual/en/pdo.construct.php
+function openDBConnexion()
+{
+    $tempDbConnexion = null;
+
+    $sqlDriver = 'mysql';
+    $hostname = 'localhost';
+    $port = 3306;
+    $charset = 'utf8';
+    $dbName = 'snows';
+    $userName = 'appliConnector';
+    $userPwd = '123qweasD$';
+    $dsn = $sqlDriver . ':host=' . $hostname . ';dbname=' . $dbName . ';port=' . $port . ';charset=' . $charset;
+
+    try {
+        $tempDbConnexion = new PDO($dsn, $userName, $userPwd);
+    } catch (PDOException $exception) {
+        echo 'Connection failed: ' . $exception->getMessage();
+    }
+    return $tempDbConnexion;
+}
---
+++ view/login.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * 151_2019_startproject - login.php
+ * User: Jan.BLATTER
+ * Date: 07.02.2020
+ */
